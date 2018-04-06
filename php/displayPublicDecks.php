ec547521f6ca87299bf6f0b3e08796775c70b88c
John <37817270+Gildazol@users.noreply.github.com>
2018-04-06 14:44:34 -0400

    Included public parameter

--- php/displayPublicDecks.php
+++ php/displayPublicDecks.php
@@ -10,10 +10,10 @@
 			$stmt->execute(); // Run query
 			$result = $stmt->get_result(); // Get the results of running the query
 			
-			if ($result->num_rows == 0) { // If user does not have any decks
+			if ($result->num_rows == 0) { // If there are no public decks
 				echo "<h3>There are no public decks yet.</h3>";
 			}
-			else { // User has 1 or more decks
+			else { // 1 or more public decks
 				while($row = $result->fetch_assoc()) { // Go through each row and fetch deck data
 				$userID = $row['userID']; // Retrieve deck title
 				$title = $row['title']; // Retrieve deck title
@@ -28,7 +28,7 @@
 				$usernameRow = $usernameResult->fetch_assoc();
 				$username = $usernameRow['username'];
 			
-				$deck = new Deck($deckID, $title, $description, $imageBlob, $userID);
+				$deck = new Deck($deckID, $title, $description, $imageBlob, $userID, "1");
 				$deck->displayPublicDeck($username);
 
 			}
