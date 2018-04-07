d06a9aee22e678dca71541c50fbde2f2188dea79
Donovan <donovan@prehn.ca>
2018-04-07 02:31:42 -0400

    comments and tab fixes

--- php/nav-bar.php
+++ php/nav-bar.php
@@ -1,6 +1,8 @@
+<!-- Static navbar, included by all the other php pages -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary mt-3 mb-3">
 	<a class="navbar-brand" href="#">Memree Flashcards</a>
 	
+	<!-- Navigational Links -->
 	<ul class="navbar-nav">
 		<li class="nav-item">
 		  <a class="nav-link" href="home.php">My Decks</a>
--- php/registerUser.php
+++ php/registerUser.php
@@ -7,16 +7,18 @@
 	$db = mysqli_connect('localhost', 'root', '', 'memree_flashcards');
 	
 	if (isset($_POST['reg_user'])) {
-		
+	  
+	  //Prevent against SQL injection
 	  $username = mysqli_real_escape_string($db, $_POST['username']);
 	  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 	  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
-
+	  
+	  //Validate input
 	  if (empty($username)) { array_push($errors, "Username is required");}
 	  if (empty($password_1)) { array_push($errors, "Password is required");}
 	  if ($password_1 != $password_2) { array_push($errors, "The two passwords do not match");}
 
-
+	  //Query DB for existing user
 	  $user_check_query = "SELECT * FROM users WHERE username='$username'LIMIT 1";
 	  $result = mysqli_query($db, $user_check_query);
 	  $user = mysqli_fetch_assoc($result);
@@ -28,11 +30,16 @@
 	  }
 
 	  if (count($errors) == 0) {
+		
+	    //Generate a random salt and use it to hash password
 		$password = password_hash($password_1, PASSWORD_BCRYPT);
+		
+		//Insert hash/salt into DB.
 		array_push($alerts, "Account successfully created!");
 		$query = "INSERT INTO users (username, password) 
 				  VALUES('$username', '$password')";
 		mysqli_query($db, $query);
+		
 		$_SESSION['username'] = $username;
 		$_SESSION['success'] = "You are now logged in";
 		header('location: index.php');
